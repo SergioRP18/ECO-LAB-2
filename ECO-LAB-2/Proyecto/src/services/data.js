bcb4955c8226bc03849a4a53833fe297fd39800e
Sergio Restrepo Prado <153745435+SergioRP18@users.noreply.github.com>
2025-02-17 09:58:17 -0500

    Add files via upload

---
+++ ECO-LAB-2/Proyecto/src/services/data.js
@@ -0,0 +1,14 @@
+const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:3004/posts');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Failed to fetch:', error);
+    }
+  };
+
+  document.getElementById("fetch-button").addEventListener("click", fetchData);
